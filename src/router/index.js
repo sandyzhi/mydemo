10d28f63be6c3af35c6caabecfa2b0fbeb63dd49
小智大愚 <dxzhishanshan@163.com>
2020-07-12 15:18:36 +0800

    sort demo

--- src/router/index.js
+++ src/router/index.js
@@ -9,68 +9,79 @@ import DynamicTable from '../views/tabledemo/dynamicTable.vue'
 import CustomTable from '../views/tabledemo/customTable.vue'
 import HBTable from '../views/tabledemo/hebingtable.vue'
 import SelectDemo from '../views/select/SelectDemo.vue'
+import UploadAndDownLoad from '../views/file/UploadAndDownload.vue'
+import SortDemo from '../views/select/SortDemo.vue'
 
 Vue.use(VueRouter)
 
 const routes = [{
-  path: '/',
-  redirect: '/selectdemo'
-},
-{
-  path: '/home',
-  name: 'Home',
-  component: Home
-},
-{
-  path: '/list',
-  name: 'List',
-  component: List
-},
-{
-  path: '/details',
-  name: 'Details',
-  component: Details
-}, {
-  path: '/one',
-  name: 'One',
-  component: One
-},
-{
-  path: '/two',
-  name: 'Two',
-  component: Two
-},
-{
-  path: '/dynamicTable',
-  name: 'dynamicTable',
-  component: DynamicTable
-},
-{
-  path: '/customTable',
-  name: 'customTable',
-  component: CustomTable
-},{
-  path:'/hebingtable',
-  name:'hebingtable',
-  component:HBTable
-},{
-  path:'/selectdemo',
-  name:'selectdemo',
-  component:SelectDemo
-}
-    // ,
-    // {
-    //   path: '/about',
-    //   name: 'About',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-    // }
+    path: '/',
+    redirect: '/sortdemo'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/list',
+    name: 'List',
+    component: List
+  },
+  {
+    path: '/details',
+    name: 'Details',
+    component: Details
+  }, {
+    path: '/one',
+    name: 'One',
+    component: One
+  },
+  {
+    path: '/two',
+    name: 'Two',
+    component: Two
+  },
+  {
+    path: '/dynamicTable',
+    name: 'dynamicTable',
+    component: DynamicTable
+  },
+  {
+    path: '/customTable',
+    name: 'customTable',
+    component: CustomTable
+  }, {
+    path: '/hebingtable',
+    name: 'hebingtable',
+    component: HBTable
+  }, {
+    path: '/selectdemo',
+    name: 'selectdemo',
+    component: SelectDemo
+  },
+  {
+    path: '/sortdemo',
+    name: 'sortdemo',
+    component: SortDemo
+  }, {
+    path: '/file',
+    name: 'uploadAndDownLoad',
+    component: UploadAndDownLoad
+  }
+  // ,
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
-    routes
+  routes
 })
 
 export default router
